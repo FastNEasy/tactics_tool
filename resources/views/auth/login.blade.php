bb95a39fc17664120b76012448785d17bf2daafa
NK <niksgrigors1@gmail.com>
2021-05-27 11:31:28 +0300

    Changes to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
                 <!-- <div class="card-header">{{ __('Login') }}</div> -->
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                    <form method="POST" class="form" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
@@ -53,7 +53,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="loginButton">
+                                <button type="submit" class="button">
                                     {{ __('Login') }}
                                 </button>
 
@@ -70,4 +70,5 @@
         </div>
     </div>
 </div>
+
 @endsection
