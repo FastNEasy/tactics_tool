39623090829c7e30d601ac092d6ad74131d48ebd
AA <aleksisabols360@gmail.com>
2021-06-03 15:48:13 +0300

    pull rename version

--- resources/js/components/Create.vue
+++ resources/js/components/Create.vue
@@ -43,6 +43,7 @@
                 },
                 onTacticAdd(){//adding tactic to the table
                     this.presetID = document.getElementById("presetsid").value;
+                    document.getElementById().validity.valid
                     sampleRequest.saveTactic({ nameTactic: this.tacticName, presetChoice: this.presetID, loginId: this.user }).then(response => {
                         console.log("Response received-2", response);
                     }).catch(error => {
